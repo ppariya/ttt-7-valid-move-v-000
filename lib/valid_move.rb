8ed8fa517139cb2d3436ffe2a2fcfefc72b68a52
Piyavadee Pariyavuth <piyavadeepariyavuth@gmail.com>
2018-01-10 22:21:55 +0000

    automatically backed up by learn

--- lib/valid_move.rb
+++ lib/valid_move.rb
@@ -1,7 +1,7 @@
 # code your #valid_move? method here
 # checking for a valid index if it is true then it will check for a position is taken or not. If both true, then it is a valid move.
 def valid_move?(board, index)
-    return index.between?(0, 8) && !(position_taken?(board, index))
+  index.between?(0, 8) && !(position_taken?(board, index))
 end
 
 # re-define your #position_taken? method here, so that you can use it in the #valid_move? method above.
